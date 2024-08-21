1861392b8570d0ebebd0d73eebe9d1fa4166db55
iverrr78 <ivandres6d@gmail.com>
2024-08-21 12:29:28 -0600

    Creation of models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    userid = models.CharField(pk = True, max_length=50)
+    username = models.CharField(max_length = 50)
+    email = models.EmailField()
+    password = models.CharField(max_length = 50)
+
+TASK_IMPORTANCE_CHOICES = [
+    'Alto': 'Alto',
+    'Medio': 'Medio',
+    'Bajo': 'Bajo'
+]
+
+TASK_STAGE_CHOICES= [
+    'En proceso': 'En proceso',
+    'Finalizado': 'Finalizado',
+    'Sin iniciar': 'Sin iniciar'
+]
+class Task(models.Model):
+    taskid = models.CharField(pk = True, max_length=50)
+    userid = models.ForeignKey(User, on_delete= models.CASCADE)
+    importance = models.CharField(max_length=20, choices=TASK_IMPORTANCE_CHOICES)
+    stage = models.CharField(max_length=20, choices= TASK_STAGE_CHOICES)
+    title = models.CharField(max_length= 50)
+    description = models.TextField()
