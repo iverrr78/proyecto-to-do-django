c180ffbb18bc121a4f60cc7c35b2397b0596ce55
iverrr78 <ivandres6d@gmail.com>
2024-08-22 23:01:08 -0600

    final commit 2

--- proyecto_to_do_django/settings.py
+++ proyecto_to_do_django/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +29,9 @@ SECRET_KEY = 'django-insecure-nhtgs@#^u09^36yqszs7r6=v6a=5uggm(tkbl$fxd0=ezxxn-e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'to-do-app-production.eba-mxznxirk.us-east-2.elasticbeanstalk.com'
+    ]
 
 
 # Application definition
@@ -76,10 +82,7 @@ WSGI_APPLICATION = 'proyecto_to_do_django.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db('DJANGO_DB_URL')
 }
 
 
